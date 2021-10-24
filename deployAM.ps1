c993621c854f782be1e0333ef9a0a1998e1243d2
Matrix20085 <mikemaster2000btfu@yahoo.com>
2021-10-23 22:01:43 -0400

    Update deployAM.ps1
    Removing autoboot for operator VMs

--- deployAM.ps1
+++ deployAM.ps1
@@ -1,60 +1,3 @@
-# Hard variables
-$serverLocation = "Servers"
-$kaliLocation = "Kalis"
-$commanodLocation = "Commandos"
-
-
-# Download and import PowerCli
-Write-Host "Downloading VMWare Tools, this may take a while..." -ForegroundColor Cyan -BackgroundColor Black
-Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force:$true
-Write-Host "Importing VMWare Tools, this may take a while..." -ForegroundColor Cyan -BackgroundColor Black
-Get-Module -ListAvailable VMware* | Import-Module | Out-Null
-cls
-
-
-# Get user information and loginto vCenter
-# Instead of exit on bad login we should loop back and allow multiple tries
-$vCenterServer = Read-Host -Prompt "IP or FQDN of vCenter server"
-$vCenterUsername = Read-Host -Prompt "vCenter username"
-$vCenterPassword = Read-Host -Prompt "vCenter password" -AsSecureString
-$vCenterPassword = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($vCenterPassword))
-Set-PowerCLIConfiguration -InvalidCertificateAction Ignore -Confirm:$false | Out-Null
-if (Connect-VIServer -Server $vCenterServer -Protocol https -User $vCenterUsername -Password $vCenterPassword -ErrorAction SilentlyContinue) {
-    Write-Output 'Connected'
-} 
-else {
-    $Error[0]
-    pause
-    exit
-}
-
-
-# Get VMHost object
-# Instead of exit on bad host we should loop back and allow multiple tries
-Write-Host `n`n VM Hosts:`n (Get-VMHost) `n
-$vmHostName = Read-Host -Prompt "Chose VM Host from the selection above: "
-$vmHost = Get-VMHost -Name $vmHostName -ErrorAction SilentlyContinue
-if (-not $vmHost) {
-    Write-Host "Host name incorrect!!!" -ForegroundColor Red -BackgroundColor Black
-    pause
-    exit
-}
-
-
-#Get Datastore object
-# Instead of exit on bad datastore we should loop back and allow multiple tries
-Write-Host `n`nData Stores:`n (Get-Datastore) `n
-$datastoreName = Read-Host -Prompt "Chose datastore from the selection above: "
-$datastore = Get-Datastore -Name $datastoreName -ErrorAction SilentlyContinue
-if (-not $datastore) {
-    Write-Host "Datastore name incorrect!!!" -ForegroundColor Red -BackgroundColor Black
-    pause
-    exit
-}
-
-$numOfOperators = Read-Host -Prompt "`n`nHow many sets of operator VMs are needed"
-
-
 # Make the folders for organization
 # Need to test where the name "vm" comes from. It might be a default just not sure
 Get-Folder -Name vm | New-Folder -Name $serverLocation -ErrorAction SilentlyContinue
@@ -74,6 +17,7 @@ $macCounter = 10
 foreach ($server in $serverList) {
     Write-Host "Deploying $server"
     $template = Get-Template -Name "$server Gold"
+    $server = "Test $server" # <--------------------------------------------------------------------------REMOVE THIS. FOR TESTING ONLY
     New-VM -Name $server -Template $template -Location $serverLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
     $currentVM = Get-VM -Name $server
     $currentNIC = Get-NetworkAdapter -VM $currentVM
@@ -92,7 +36,6 @@ for ($i=1 ; $i -le $numOfOperators ; $i++) {
     $currentVM = Get-VM -Name "Kali $i"
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
-    Get-VMStartPolicy -VM $currentVM | Set-VMStartPolicy -StartAction PowerOn | Out-Null
     $macCounter++
 }
 
@@ -106,6 +49,5 @@ for ($i=1 ; $i -le $numOfOperators ; $i++) {
     $currentVM = Get-VM -Name "Commando $i"
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
-    Get-VMStartPolicy -VM $currentVM | Set-VMStartPolicy -StartAction PowerOn | Out-Null
     $macCounter++
 }

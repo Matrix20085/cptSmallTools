171600a53d74354a229b52126bfdac81170cc235
Matrix20085 <mikemaster2000btfu@yahoo.com>
2021-10-27 00:14:46 -0400

    Update deployAM.ps1

--- deployAM.ps1
+++ deployAM.ps1
@@ -27,7 +27,7 @@ if ($host.name -match 'Windows PowerShell ISE Host')
 
 # Download and import PowerCli
 Write-Host "Downloading VMWare Tools, this may take a while..." -ForegroundColor Cyan -BackgroundColor Black
-Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force:$true
+Install-Module -Name VMware.PowerCLI -Scope CurrentUser -Force:$true -SkipPublisherCheck -AllowClobber
 Write-Host "Importing VMWare Tools, this may take a while..." -ForegroundColor Cyan -BackgroundColor Black
 Get-Module -ListAvailable VMware* | Import-Module | Out-Null
 cls
@@ -56,7 +56,8 @@ $guestPassword = Read-Host -Prompt "Password for CPT account on Kali and Command
 
 # Get VMHost object
 # Instead of exit on bad host we should loop back and allow multiple tries
-Write-Host `n`n VM Hosts:`n (Get-VMHost) `n
+Write-Host `n`n VM Hosts:`n
+foreach ($hostName in Get-VMHost) { Write-Host $hostName }
 $vmHostName = Read-Host -Prompt "Chose VM Host from the selection above: "
 $vmHost = Get-VMHost -Name $vmHostName -ErrorAction SilentlyContinue
 if (-not $vmHost) {
@@ -68,7 +69,8 @@ if (-not $vmHost) {
 
 # Get Datastore object
 # Instead of exit on bad datastore we should loop back and allow multiple tries
-Write-Host `n`nData Stores:`n (Get-Datastore) `n
+Write-Host `n`nData Stores:`n
+foreach ($datastName in ($vmHost | Get-Datastore)) { Write-Host $datastName }
 $datastoreName = Read-Host -Prompt "Chose datastore from the selection above: "
 $datastore = Get-Datastore -Name $datastoreName -ErrorAction SilentlyContinue
 if (-not $datastore) {
@@ -94,8 +96,8 @@ Get-Folder -Name vm | New-Folder -Name $kaliLocation -ErrorAction SilentlyContin
 Get-Folder -Name vm | New-Folder -Name $commanodLocation -ErrorAction SilentlyContinue
 
 
-# Set switch vSwitch0 uplink to vmnic0
-Get-VirtualSwitch -Name "vSwitch0" | Set-VirtualSwitch -Nic "vmnic0"  # Might need to use Add/Remove-VirtualSwitchPhysicalNetworkAdapter
+# Set switch vSwitch0 uplink to vmnic5
+Get-VirtualSwitch -Name "vSwitch0" | Set-VirtualSwitch -Nic "vmnic5"  # Might need to use Add/Remove-VirtualSwitchPhysicalNetworkAdapter
 
 # Make virtual switch cpt.local with uplink vmnic3 and vmnic4
 New-VirtualSwitch -Name "cpt.local" -Nic (Get-VMHostNetworkAdapter -Name "vmnic3") -VMHost $vmHost | Out-Null
@@ -104,9 +106,6 @@ Add-VirtualSwitchPhysicalNetworkAdapter -VirtualSwitch (Get-VirtualSwitch -Name 
 # Make virtual swtich Cell Router with uplink vmnic1
 New-VirtualSwitch -Name "Cell Router" -Nic (Get-VMHostNetworkAdapter -Name "vmnic1") -VMHost $vmHost | Out-Null
 
-# Make virtual switch Emergency with uplink vmnic5
-New-VirtualSwitch -Name "Emergency" -Nic (Get-VMHostNetworkAdapter -Name "vmnic5") -VMHost $vmHost | Out-Null
-
 # Make portgroup cpt.local assigned to cpt.local
 New-VirtualPortGroup -Name "cpt.local" -VirtualSwitch (Get-VirtualSwitch -Name "cpt.local") | Out-Null
 
@@ -114,13 +113,10 @@ New-VirtualPortGroup -Name "cpt.local" -VirtualSwitch (Get-VirtualSwitch -Name "
 New-VirtualPortGroup -Name "cpt.local Management" -VirtualSwitch (Get-VirtualSwitch -Name "cpt.local") | Out-Null
 
 # Check portgroup VM Network assigned to vSwitch0
-New-VirtualPortGroup -Name "VM Network" -VirtualSwitch (Get-VirtualSwitch -Name "vSwitch0") | Out-Null
+New-VirtualPortGroup -Name "Target" -VirtualSwitch (Get-VirtualSwitch -Name "Target") | Out-Null
 
 # Make portgroup Cell Router assigned to Cell Router
 New-VirtualPortGroup -Name "Cell Router" -VirtualSwitch (Get-VirtualSwitch -Name "Cell Router") | Out-Null
-
-# Make portgroup Emergency Management assigned to Emergency
-New-VirtualPortGroup -Name "Emergency Management" -VirtualSwitch (Get-VirtualSwitch -Name "Emergency") | Out-Null
 
 # Make VMKernalNic with portgroup cpt.local Management with static 172.20.20.2 (255.255.255.0) add service management
 New-VMHostNetworkAdapter -PortGroup (Get-VirtualPortGroup -Name "cpt.local Management") -VirtualSwitch (Get-VirtualSwitch -Name "cpt.local") -ManagementTrafficEnabled $true -IP "172.20.20.2" -SubnetMask "255.255.255.0" | Out-Null
@@ -129,13 +125,16 @@ New-VMHostNetworkAdapter -PortGroup (Get-VirtualPortGroup -Name "cpt.local Manag
 New-VMHostNetworkAdapter -PortGroup (Get-VirtualPortGroup -Name "Emergency Management") -VirtualSwitch (Get-VirtualSwitch -Name "Emergency") -ManagementTrafficEnabled $true -IP "172.17.90.1" -SubnetMask "255.255.255.0" | Out-Null
 
 
+Get-VMHostStartPolicy -VMHost $vmHost | Set-VMHostStartPolicy -Enabled $true -StartDelay 120 -WaitForHeartBeat $true | Out-Null
+
+
 # Deploying pfSense
 Write-Host "Deploying pfSense"
 $template = Get-Template -Name "pfSense Gold"
-$server = "Test pfSense" # <--------------------------------------------------------------------------REMOVE THIS. FOR TESTING ONLY
-New-VM -Name $server -Template $template -Location $serverLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
-$currentVM = Get-VM -Name $server
-Get-VMStartPolicy -VM $currentVM | Set-VMStartPolicy -StartAction PowerOn -StartOrder 1 -StartDelay 120 | Out-Null
+$server = "pfSense"
+New-VM -Name $server -Template $template -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
+$currentVM = Get-VM -Name $server -Datastore $datastore
+Get-VMStartPolicy -VM $currentVM | Set-VMStartPolicy -StartAction PowerOn -StartOrder 1 -StartDelay 120| Out-Null
 
 
 # List of all servers.
@@ -147,24 +146,21 @@ $macCounter = 10
 foreach ($server in $serverList) {
     Write-Host "Deploying $server"
     $template = Get-Template -Name "$server Gold"
-    $server = "Test $server" # <--------------------------------------------------------------------------REMOVE THIS. FOR TESTING ONLY
-    New-VM -Name $server -Template $template -Location $serverLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
-    $currentVM = Get-VM -Name $server
+    $server = $server
+    New-VM -Name $server -Template $template -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
+    $currentVM = Get-VM -Name $server -Datastore $datastore
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
-    Get-VMStartPolicy -VM $currentVM | Set-VMStartPolicy -StartAction PowerOn -StartDelay 300 | Out-Null
+    Get-VMStartPolicy -VM $currentVM | Set-VMStartPolicy -StartAction PowerOn | Out-Null
     $macCounter++
 }
 
 
-# Add to deployment "sudo sed -i 's/kali/kali-$i/g' /etc/hosts" using Invoke-VMScript
-# Need to bootup host run this, then shutdown
-
 # Deploying CPT Kali 
 Write-Host "Deploying CPT-Kali"
 $template = Get-Template -Name "Kali Gold"
-New-VM -Name "CPT-Kali" -Template $template -Location $kaliLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
-$currentVM = Get-VM -Name "CPT-Kali"
+New-VM -Name "CPT-Kali" -Template $template -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
+$currentVM = Get-VM -Name "CPT-Kali"  -Datastore $datastore
 $currentNIC = Get-NetworkAdapter -VM $currentVM
 Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:21" -Confirm:$false | Out-Null
 New-NetworkAdapter -VM $currentVM -StartConnected -NetworkName "VM Network" | Out-Null
@@ -175,11 +171,11 @@ Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
 
 
 # Deploying Kali
-$macCounter = 30g
+$macCounter = 30
 for ($i=0 ; $i -lt $numOfOperators ; $i++) {
     Write-Host "Deploying Kali-$i"
-    New-VM -Name "Kali-$i" -Template $template -Location $kaliLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
-    $currentVM = Get-VM -Name "Kali-$i"
+    New-VM -Name "Kali-$i" -Template $template -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
+    $currentVM = Get-VM -Name "Kali-$i"  -Datastore $datastore
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
     Start-VM -VM $currentVM | Out-Null
@@ -193,8 +189,8 @@ for ($i=0 ; $i -lt $numOfOperators ; $i++) {
 #Deploying CPT Commanod
 Write-Host "Deploying CPT-Commando"
 $template = Get-Template -Name "Commando Gold"
-New-VM -Name "CPT-Commando" -Template $template -Location $commanodLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
-$currentVM = Get-VM -Name "CPT-Commando"
+New-VM -Name "CPT-Commando" -Template $template -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
+$currentVM = Get-VM -Name "CPT-Commando"  -Datastore $datastore
 $currentNIC = Get-NetworkAdapter -VM $currentVM
 Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:22" -Confirm:$false | Out-Null
 New-NetworkAdapter -VM $currentVM -StartConnected -NetworkName "VM Network" | Out-Null
@@ -208,8 +204,8 @@ Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
 $macCounter = 40
 for ($i=0 ; $i -lt $numOfOperators ; $i++) {
     Write-Host "Deploying Commando-$i"
-    New-VM -Name "Commando-$i" -Template $template -Location $commanodLocation -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
-    $currentVM = Get-VM -Name "Commando-$i"
+    New-VM -Name "Commando-$i" -Template $template -Datastore $datastore -DiskStorageFormat Thin -VMHost $vmHost | Out-Null
+    $currentVM = Get-VM -Name "Commando-$i"  -Datastore $datastore
     $currentNIC = Get-NetworkAdapter -VM $currentVM
     Set-NetworkAdapter -NetworkAdapter $currentNIC -MacAddress "00:50:56:17:90:$macCounter" -Confirm:$false | Out-Null
     Start-VM -VM $currentVM | Out-Null
@@ -218,3 +214,4 @@ for ($i=0 ; $i -lt $numOfOperators ; $i++) {
     Shutdown-VMGuest -VM $currentVM -Confirm:$false | Out-Null
     $macCounter++
 }
+pause
